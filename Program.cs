d0914c4c18716b10d295bce33892bcae5f152dbd
eyluldurus <eyll.durus@gmail.com>
2024-10-08 18:41:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//string ,int ,float,double,bool;
+string yazi1;string yazi2;string yazi3;int ortalama;
+
+yazi1 = "Lütfen Birinci Dersin Notunu Giriniz.";
+Console.WriteLine(yazi1);
+string ders1 = Console.ReadLine();
+
+yazi2 = "Lütfen İkinci Dersin Notunu Giriniz.";
+Console.WriteLine(yazi2);
+string ders2 = Console.ReadLine();
+
+yazi3 = "Lütfen Üçüncü Dersin Notunu Giriniz.";
+Console.WriteLine(yazi3);
+string ders3 = Console.ReadLine();
+
+ortalama = int.Parse(ders1)+int.Parse(ders2)+int.Parse(ders3);
+ortalama = ortalama/3;
+
+if(ortalama >= 50){
+    Console.WriteLine("---*****---Bu öğrenci geçti. Ortalaması:"+ortalama+"---*****---");
+}else{
+    Console.WriteLine("---*****---Bu öğrenci kaldı. Ortalaması:"+ortalama+"---*****---");
+}
+
